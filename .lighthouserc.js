3fb5e5a146f26958ba03c42e48bb95a8941aab50
moekidev <me@moeki.dev>
2022-01-08 01:10:24 +0900

    disable lighthouse ci recommend rules

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,9 +7,6 @@ module.exports = {
       target: "lhci",
       serverBaseUrl: "https://lhci-moekidev.herokuapp.com",
       token: "de02dce2-36ec-413e-b1c5-39e51d020f71"
-    },
-    assert: {
-      preset: 'lighthouse:recommended'
     }
   }
 }
